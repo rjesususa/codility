847d5c8beee5819d9a21b57c34af6959fa819dfe
Rafael Neves <rjesus@thoughtworks.com>
2018-05-07 17:54:01 -0300

    Created tests and implemented private method to return the max repeatable value in the tree

--- src/main/java/br/com/codility/Tree.java
+++ src/main/java/br/com/codility/Tree.java
@@ -1,5 +1,8 @@
 package br.com.codility;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static java.lang.Integer.max;
 import static java.util.Objects.isNull;
 
@@ -9,8 +12,8 @@ import static java.util.Objects.isNull;
 public class Tree {
     private Tree parent;
     private Integer value;
-    private Tree leftNodes;
-    private Tree rightNodes;
+    private Tree leftNode;
+    private Tree rightNode;
 
     public Tree(Integer value) {
         this.value = value;
@@ -37,32 +40,32 @@ public class Tree {
         this.value = value;
     }
 
-    public Tree getLeftNodes() {
-        return leftNodes;
+    public Tree getLeftNode() {
+        return leftNode;
     }
 
-    public void setLeftNodes(Tree leftNodes) {
-        this.leftNodes = leftNodes;
+    public void setLeftNode(Tree leftNode) {
+        this.leftNode = leftNode;
     }
 
-    public Tree getRightNodes() {
-        return rightNodes;
+    public Tree getRightNode() {
+        return rightNode;
     }
 
-    public void setRightNodes(Tree rightNodes) {
-        this.rightNodes = rightNodes;
+    public void setRightNode(Tree rightNode) {
+        this.rightNode = rightNode;
     }
 
     private int maxValue(int value) {
-        int maxValue = max(value, isNull(leftNodes) ? 0 : leftNodes.value);
-        maxValue = max(maxValue, isNull(rightNodes) ? 0 : rightNodes.value);
+        int maxValue = max(value, isNull(leftNode) ? 0 : leftNode.value);
+        maxValue = max(maxValue, isNull(rightNode) ? 0 : rightNode.value);
 
-        if (!isNull(rightNodes)) {
-            maxValue = rightNodes.maxValue(maxValue);
+        if (!isNull(rightNode)) {
+            maxValue = rightNode.maxValue(maxValue);
         }
 
-        if (!isNull(leftNodes)) {
-            maxValue = leftNodes.maxValue(maxValue);
+        if (!isNull(leftNode)) {
+            maxValue = leftNode.maxValue(maxValue);
         }
 
         return maxValue;
@@ -73,6 +76,57 @@ public class Tree {
     }
 
     public int maxRepeatableValue() {
-        return 0;
+        Map<Integer, Integer> leftNodeValues = new HashMap<>();
+        Map<Integer, Integer> rightNodeValues = new HashMap<>();
+
+        int keyLeftMaxValue = maxRepeatableValue(leftNodeValues, leftNode);
+        int keyRightMaxValue = maxRepeatableValue(rightNodeValues, rightNode);
+
+        final Integer leftNodeMaxNumberOfIterations = leftNodeValues.get(keyLeftMaxValue) == null ? 0: leftNodeValues.get(keyLeftMaxValue);
+        final Integer rightNodeMaxNumberOfIterations = rightNodeValues.get(keyRightMaxValue) == null ? 0 : rightNodeValues.get(keyRightMaxValue);
+
+        if (leftNodeMaxNumberOfIterations > rightNodeMaxNumberOfIterations) {
+            return keyLeftMaxValue;
+        }
+
+        return keyRightMaxValue;
+    }
+
+    private int maxRepeatableValue(Map<Integer, Integer> nodeValues, Tree node) {
+
+        if (node != null) {
+            Integer previousExistingValue = nodeValues.get(node.getValue()) == null ? 0 : nodeValues.get(node.getValue());
+            nodeValues.put(node.getValue(), ++previousExistingValue);
+
+
+            if (node.leftNode != null) {
+                Integer previousLeftExistingValue = nodeValues.get(node.leftNode.getValue()) == null ? 0 : nodeValues.get(node.leftNode.getValue());
+                nodeValues.put(node.leftNode.getValue(), ++previousLeftExistingValue);
+            }
+
+            if (node.rightNode != null) {
+                Integer previousRightExistingValue = nodeValues.get(node.rightNode.getValue()) == null ? 0 : nodeValues.get(node.rightNode.getValue());
+                nodeValues.put(node.rightNode.getValue(), ++previousRightExistingValue);
+            }
+
+            if (node.leftNode != null) {
+                node.leftNode.maxRepeatableValue(nodeValues, node.leftNode);
+            }
+
+            if (node.rightNode != null) {
+                node.rightNode.maxRepeatableValue(nodeValues, node.rightNode);
+            }
+        }
+
+        final Integer[] maxRepeatableInteraction = {0};
+        final Integer[] keyOfMaxRepeatableInteraction = {0};
+        nodeValues.forEach((key, value) -> {
+            if (value > maxRepeatableInteraction[0]) {
+                maxRepeatableInteraction[0] = value;
+                keyOfMaxRepeatableInteraction[0] = key;
+            }
+        });
+
+        return keyOfMaxRepeatableInteraction[0];
     }
 }
--- src/test/java/br/com/codility/TreeTest.java
+++ src/test/java/br/com/codility/TreeTest.java
@@ -13,8 +13,8 @@ public class TreeTest {
         Tree root = new Tree(0);
         final Tree leftNode1 = new Tree(root, 1);
         Tree leftNode2 = new Tree(leftNode1, 5);
-        leftNode1.setLeftNodes(leftNode2);
-        root.setLeftNodes(leftNode1);
+        leftNode1.setLeftNode(leftNode2);
+        root.setLeftNode(leftNode1);
 
         assertThat(root.maxValue(), equalTo(5));
     }
@@ -27,9 +27,9 @@ public class TreeTest {
         Tree rightNode2 = new Tree(rightNode1, 7);
         Tree rightNode3 = new Tree(rightNode2, 2);
 
-        rightNode2.setRightNodes(rightNode3);
-        rightNode1.setRightNodes(rightNode2);
-        root.setRightNodes(rightNode1);
+        rightNode2.setRightNode(rightNode3);
+        rightNode1.setRightNode(rightNode2);
+        root.setRightNode(rightNode1);
 
 
         assertThat(root.maxValue(), is(7));
@@ -40,16 +40,16 @@ public class TreeTest {
         Tree root = new Tree(0);
         final Tree leftNode1 = new Tree(root, 5);
         Tree leftNode2 = new Tree(leftNode1, 2);
-        leftNode1.setLeftNodes(leftNode2);
-        root.setLeftNodes(leftNode1);
+        leftNode1.setLeftNode(leftNode2);
+        root.setLeftNode(leftNode1);
 
         Tree rightNode1 = new Tree(root, 3);
         Tree rightNode2 = new Tree(rightNode1, 6);
         Tree rightNode3 = new Tree(rightNode2, 8);
 
-        rightNode2.setRightNodes(rightNode3);
-        rightNode1.setRightNodes(rightNode2);
-        root.setRightNodes(rightNode1);
+        rightNode2.setRightNode(rightNode3);
+        rightNode1.setRightNode(rightNode2);
+        root.setRightNode(rightNode1);
 
 
         assertThat(root.maxValue(), is(8));
@@ -61,17 +61,17 @@ public class TreeTest {
         final Tree leftNode1 = new Tree(root, 5);
         Tree leftNode2 = new Tree(leftNode1, 2);
         Tree rightNode11 = new Tree(leftNode2, 9);
-        leftNode2.setRightNodes(rightNode11);
-        leftNode1.setLeftNodes(leftNode2);
-        root.setLeftNodes(leftNode1);
+        leftNode2.setRightNode(rightNode11);
+        leftNode1.setLeftNode(leftNode2);
+        root.setLeftNode(leftNode1);
 
         Tree rightNode1 = new Tree(root, 3);
         Tree rightNode2 = new Tree(rightNode1, 8);
         Tree rightNode3 = new Tree(rightNode2, 7);
 
-        rightNode2.setRightNodes(rightNode3);
-        rightNode1.setRightNodes(rightNode2);
-        root.setRightNodes(rightNode1);
+        rightNode2.setRightNode(rightNode3);
+        rightNode1.setRightNode(rightNode2);
+        root.setRightNode(rightNode1);
 
 
         assertThat(root.maxValue(), is(9));
@@ -86,27 +86,27 @@ public class TreeTest {
         Tree rightNode11 = new Tree(leftNode2, 9);
         Tree left2Node1 = new Tree(rightNode11, 3);
         Tree right3Node2 = new Tree(left2Node1, 4);
-        left2Node1.setRightNodes(right3Node2);
+        left2Node1.setRightNode(right3Node2);
         Tree left3Node2 = new Tree(left2Node1, 9);
         Tree expectedNodeValue = new Tree(left3Node2, 10);
-        left3Node2.setRightNodes(expectedNodeValue);
-        left2Node1.setLeftNodes(left3Node2);
+        left3Node2.setRightNode(expectedNodeValue);
+        left2Node1.setLeftNode(left3Node2);
         Tree right2Node1 = new Tree(rightNode11, 5);
 
-        rightNode11.setLeftNodes(left2Node1);
-        rightNode11.setRightNodes(right2Node1);
+        rightNode11.setLeftNode(left2Node1);
+        rightNode11.setRightNode(right2Node1);
 
-        leftNode2.setRightNodes(rightNode11);
-        leftNode1.setLeftNodes(leftNode2);
-        root.setLeftNodes(leftNode1);
+        leftNode2.setRightNode(rightNode11);
+        leftNode1.setLeftNode(leftNode2);
+        root.setLeftNode(leftNode1);
 
         Tree rightNode1 = new Tree(root, 3);
         Tree rightNode2 = new Tree(rightNode1, 8);
         Tree rightNode3 = new Tree(rightNode2, 7);
 
-        rightNode2.setRightNodes(rightNode3);
-        rightNode1.setRightNodes(rightNode2);
-        root.setRightNodes(rightNode1);
+        rightNode2.setRightNode(rightNode3);
+        rightNode1.setRightNode(rightNode2);
+        root.setRightNode(rightNode1);
 
 
         assertThat(root.maxValue(), is(10));
@@ -121,27 +121,27 @@ public class TreeTest {
         Tree rightNode11 = new Tree(leftNode2, 9);
         Tree left2Node1 = new Tree(rightNode11, 3);
         Tree right3Node2 = new Tree(left2Node1, 4);
-        left2Node1.setRightNodes(right3Node2);
+        left2Node1.setRightNode(right3Node2);
         Tree left3Node2 = new Tree(left2Node1, 9);
         Tree expectedNodeValue = new Tree(left3Node2, 10);
-        left3Node2.setRightNodes(expectedNodeValue);
-        left2Node1.setLeftNodes(left3Node2);
+        left3Node2.setRightNode(expectedNodeValue);
+        left2Node1.setLeftNode(left3Node2);
         Tree right2Node1 = new Tree(rightNode11, 5);
 
-        rightNode11.setLeftNodes(left2Node1);
-        rightNode11.setRightNodes(right2Node1);
+        rightNode11.setLeftNode(left2Node1);
+        rightNode11.setRightNode(right2Node1);
 
-        leftNode2.setRightNodes(rightNode11);
-        leftNode1.setLeftNodes(leftNode2);
-        root.setLeftNodes(leftNode1);
+        leftNode2.setRightNode(rightNode11);
+        leftNode1.setLeftNode(leftNode2);
+        root.setLeftNode(leftNode1);
 
         Tree rightNode1 = new Tree(root, 3);
         Tree rightNode2 = new Tree(rightNode1, 8);
         Tree rightNode3 = new Tree(rightNode2, 11);
 
-        rightNode2.setRightNodes(rightNode3);
-        rightNode1.setRightNodes(rightNode2);
-        root.setRightNodes(rightNode1);
+        rightNode2.setRightNode(rightNode3);
+        rightNode1.setRightNode(rightNode2);
+        root.setRightNode(rightNode1);
 
 
         assertThat(root.maxValue(), is(11));
@@ -156,19 +156,19 @@ public class TreeTest {
         Tree rightNode11 = new Tree(leftNode2, 9);
         Tree left2Node1 = new Tree(rightNode11, 3);
         Tree right3Node2 = new Tree(left2Node1, 4);
-        left2Node1.setRightNodes(right3Node2);
+        left2Node1.setRightNode(right3Node2);
         Tree left3Node2 = new Tree(left2Node1, 9);
         Tree expectedNodeValue = new Tree(left3Node2, 10);
-        left3Node2.setRightNodes(expectedNodeValue);
-        left2Node1.setLeftNodes(left3Node2);
+        left3Node2.setRightNode(expectedNodeValue);
+        left2Node1.setLeftNode(left3Node2);
         Tree right2Node1 = new Tree(rightNode11, 5);
 
-        rightNode11.setLeftNodes(left2Node1);
-        rightNode11.setRightNodes(right2Node1);
+        rightNode11.setLeftNode(left2Node1);
+        rightNode11.setRightNode(right2Node1);
 
-        leftNode2.setRightNodes(rightNode11);
-        leftNode1.setLeftNodes(leftNode2);
-        root.setLeftNodes(leftNode1);
+        leftNode2.setRightNode(rightNode11);
+        leftNode1.setLeftNode(leftNode2);
+        root.setLeftNode(leftNode1);
 
         Tree rightNode1 = new Tree(root, 3);
         Tree rightNode2 = new Tree(rightNode1, 8);
@@ -177,20 +177,20 @@ public class TreeTest {
         Tree right3Node1 = new Tree(rightNode3, 2);
         Tree right4Node1 = new Tree(right3Node1, 9);
         Tree left4Node1 = new Tree(right3Node1, 13);
-        right3Node1.setRightNodes(right4Node1);
-        right3Node1.setLeftNodes(left4Node1);
+        right3Node1.setRightNode(right4Node1);
+        right3Node1.setLeftNode(left4Node1);
 
         Tree left3Node1 = new Tree(rightNode3, 6);
         Tree right5Node1 = new Tree(left3Node1, 15);
         Tree left5Node1 = new Tree(left3Node1, 14);
-        left3Node1.setLeftNodes(left5Node1);
-        left3Node1.setRightNodes(right5Node1);
-        rightNode3.setRightNodes(right3Node1);
-        rightNode3.setLeftNodes(left3Node1);
+        left3Node1.setLeftNode(left5Node1);
+        left3Node1.setRightNode(right5Node1);
+        rightNode3.setRightNode(right3Node1);
+        rightNode3.setLeftNode(left3Node1);
 
-        rightNode2.setRightNodes(rightNode3);
-        rightNode1.setRightNodes(rightNode2);
-        root.setRightNodes(rightNode1);
+        rightNode2.setRightNode(rightNode3);
+        rightNode1.setRightNode(rightNode2);
+        root.setRightNode(rightNode1);
 
 
         assertThat(root.maxValue(), is(15));
@@ -209,28 +209,112 @@ public class TreeTest {
         Tree leftNodeValue05 = new Tree(rightNodeValue7, 5);
         Tree rightNodeValue03 = new Tree(rightNodeValue7, 3);
 
-        rightNodeValue7.setRightNodes(rightNodeValue03);
-        rightNodeValue7.setLeftNodes(leftNodeValue05);
-        rightNodeValue3.setRightNodes(rightNodeValue7);
-        rightNodeValue3.setLeftNodes(leftNodeValue1);
-        leftNodeValue5.setRightNodes(rightNodeValue3);
-        leftNodeValue5.setLeftNodes(leftNodeValue4);
-        leftNodeValue3.setLeftNodes(leftNodeValue5);
-        root.setLeftNodes(leftNodeValue3);
+        rightNodeValue7.setRightNode(rightNodeValue03);
+        rightNodeValue7.setLeftNode(leftNodeValue05);
+        rightNodeValue3.setRightNode(rightNodeValue7);
+        rightNodeValue3.setLeftNode(leftNodeValue1);
+        leftNodeValue5.setRightNode(rightNodeValue3);
+        leftNodeValue5.setLeftNode(leftNodeValue4);
+        leftNodeValue3.setLeftNode(leftNodeValue5);
+        root.setLeftNode(leftNodeValue3);
 
         Tree rightNodeValue9 = new Tree(root, 9);
         Tree rightNodeValue10 = new Tree(rightNodeValue9, 10);
         Tree leftNodeValue2 = new Tree(rightNodeValue9, 2);
         Tree leftNodeValue8 = new Tree(leftNodeValue2, 8);
         Tree rightNodeValue5 = new Tree(leftNodeValue2, 5);
+        Tree leftNodeValue10 = new Tree(rightNodeValue5, 10);
 
-        leftNodeValue2.setRightNodes(rightNodeValue5);
-        leftNodeValue2.setLeftNodes(leftNodeValue8);
-        rightNodeValue9.setLeftNodes(leftNodeValue2);
-        rightNodeValue9.setRightNodes(rightNodeValue10);
-        root.setRightNodes(rightNodeValue9);
+        rightNodeValue5.setLeftNode(leftNodeValue10);
+        leftNodeValue2.setRightNode(rightNodeValue5);
+        leftNodeValue2.setLeftNode(leftNodeValue8);
+        rightNodeValue9.setLeftNode(leftNodeValue2);
+        rightNodeValue9.setRightNode(rightNodeValue10);
+        root.setRightNode(rightNodeValue9);
 
         assertThat(root.maxRepeatableValue(), is(3));
+    }
+
+    @Test
+    public void returnsTheMaxRepeatableValue10WhenTreeHasNodesWithManyLeftAndRightDirectionsAndTheSameRepeatableNumbers() {
+        Tree root = new Tree(0);
+
+        Tree leftNodeValue3 = new Tree(root, 3);
+        Tree leftNodeValue5 = new Tree(leftNodeValue3, 5);
+        Tree leftNodeValue4 = new Tree(leftNodeValue5, 4);
+        Tree rightNodeValue3 = new Tree(leftNodeValue5, 3);
+        Tree leftNodeValue1 = new Tree(rightNodeValue3, 1);
+        Tree rightNodeValue7 = new Tree(rightNodeValue3, 7);
+        Tree leftNodeValue05 = new Tree(rightNodeValue7, 5);
+        Tree rightNodeValue03 = new Tree(rightNodeValue7, 3);
+
+        rightNodeValue7.setRightNode(rightNodeValue03);
+        rightNodeValue7.setLeftNode(leftNodeValue05);
+        rightNodeValue3.setRightNode(rightNodeValue7);
+        rightNodeValue3.setLeftNode(leftNodeValue1);
+        leftNodeValue5.setRightNode(rightNodeValue3);
+        leftNodeValue5.setLeftNode(leftNodeValue4);
+        leftNodeValue3.setLeftNode(leftNodeValue5);
+        root.setLeftNode(leftNodeValue3);
+
+        Tree rightNodeValue9 = new Tree(root, 9);
+        Tree rightNodeValue10 = new Tree(rightNodeValue9, 10);
+        Tree leftNodeValue2 = new Tree(rightNodeValue9, 2);
+        Tree leftNodeValue8 = new Tree(leftNodeValue2, 8);
+        Tree rightNodeValue5 = new Tree(leftNodeValue2, 5);
+        Tree leftNodeValue10 = new Tree(rightNodeValue5, 10);
+        Tree rightNodeValue010 = new Tree(leftNodeValue10, 10);
+
+        leftNodeValue10.setRightNode(rightNodeValue010);
+        rightNodeValue5.setLeftNode(leftNodeValue10);
+        leftNodeValue2.setRightNode(rightNodeValue5);
+        leftNodeValue2.setLeftNode(leftNodeValue8);
+        rightNodeValue9.setLeftNode(leftNodeValue2);
+        rightNodeValue9.setRightNode(rightNodeValue10);
+        root.setRightNode(rightNodeValue9);
+
+        assertThat(root.maxRepeatableValue(), is(10));
+    }
+
+    @Test
+    public void returnsTheMaxRepeatableValue5WhenTreeHasNodesJustInLeftDirections() {
+        Tree root = new Tree(0);
+
+        final Tree leftNode1 = new Tree(root, 5);
+        Tree leftNode2 = new Tree(leftNode1, 2);
+        Tree rightNode10 = new Tree(leftNode1, 10);
+        Tree leftNode05 = new Tree(rightNode10, 5);
+        Tree rightNode3 = new Tree(rightNode10, 3);
+
+        rightNode10.setRightNode(rightNode3);
+        rightNode10.setLeftNode(leftNode05);
+        leftNode1.setRightNode(rightNode10);
+        leftNode1.setLeftNode(leftNode2);
+        root.setLeftNode(leftNode1);
+
+
+        assertThat(root.maxRepeatableValue(), is(5));
+    }
+
+    @Test
+    public void returnsTheMaxRepeatableValue5WhenTreeHasNodesJustInRightDirections() {
+        Tree root = new Tree(0);
+
+        final Tree rightNode1 = new Tree(root, 5);
+        Tree leftNode2 = new Tree(rightNode1, 2);
+        Tree rightNode10 = new Tree(rightNode1, 10);
+        Tree leftNode05 = new Tree(rightNode10, 5);
+        Tree rightNode3 = new Tree(rightNode10, 3);
+
+        rightNode10.setRightNode(rightNode3);
+        rightNode10.setLeftNode(leftNode05);
+        rightNode1.setRightNode(rightNode10);
+        rightNode1.setLeftNode(leftNode2);
+
+        root.setRightNode(rightNode1);
+
+
+        assertThat(root.maxRepeatableValue(), is(5));
     }
 
 }
