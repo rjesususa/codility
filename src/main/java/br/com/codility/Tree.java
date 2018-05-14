63b384cdfb2b9aa7afddafecbf31150da19764a4
Rafael Neves <rjesus@thoughtworks.com>
2018-05-14 13:15:50 -0300

    removed unused methods

--- src/main/java/br/com/codility/Tree.java
+++ src/main/java/br/com/codility/Tree.java
@@ -9,6 +9,7 @@ import static java.lang.Integer.max;
  * Created by Raneves on 22/03/18.
  */
 public class Tree {
+
     private Tree parent;
     private Integer value;
     private Tree leftNode;
@@ -23,32 +24,12 @@ public class Tree {
         this.value = value;
     }
 
-    public Tree getParent() {
-        return parent;
-    }
-
-    public void setParent(Tree parent) {
-        this.parent = parent;
-    }
-
     public int getValue() {
         return value;
     }
 
-    public void setValue(Integer value) {
-        this.value = value;
-    }
-
-    public Tree getLeftNode() {
-        return leftNode;
-    }
-
     public void setLeftNode(Tree leftNode) {
         this.leftNode = leftNode;
-    }
-
-    public Tree getRightNode() {
-        return rightNode;
     }
 
     public void setRightNode(Tree rightNode) {
@@ -105,6 +86,7 @@ public class Tree {
     private int retrieveKeyOfTheMostRepeatedValueFromNodes(Map<Integer, Integer> nodeValues) {
         final Integer[] maxRepeatableInteraction = {0};
         final Integer[] keyOfMaxRepeatableInteraction = {0};
+
         nodeValues.forEach((key, value) -> {
             if (value > maxRepeatableInteraction[0]) {
                 maxRepeatableInteraction[0] = value;
@@ -120,7 +102,6 @@ public class Tree {
         if (node != null) {
             Integer previousExistingValue = nodeValues.get(node.getValue()) == null ? 0 : nodeValues.get(node.getValue());
             nodeValues.put(node.getValue(), ++previousExistingValue);
-
 
             if (node.leftNode != null) {
                 final int keyOfLeftNodeValue = node.leftNode.getValue();
