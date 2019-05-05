1009699cfbb93f475525060a370706b46b54f56c
Rafael Neves <965365+rjesususa@users.noreply.github.com>
2019-05-05 19:01:46 -0400

    Created new class to assert BinaryGap algorithm

---
+++ src/main/java/br/com/codility/BinaryGapCodingTask.java
@@ -0,0 +1,18 @@
+package br.com.codility;
+
+/**
+ * Created by rjesus on 05/05/19.
+ */
+public class BinaryGapCodingTask {
+
+    int number;
+
+    public BinaryGapCodingTask(int number){
+
+        this.number = number;
+    }
+
+    public Integer getTheLongestBinaryGap() {
+        return null;
+    }
+}
---
+++ src/test/java/br/com/codility/BinaryGapCodingTaskTest.java
@@ -0,0 +1,27 @@
+package br.com.codility;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Created by rjesus on 05/05/19.
+ */
+public class BinaryGapCodingTaskTest {
+
+
+    @Test
+    public void returns0WhenTheNumberDoesntHaveBinaryGap() {
+        BinaryGapCodingTask binaryGapCodingTask = new BinaryGapCodingTask(32);
+        Integer binaryGap = binaryGapCodingTask.getTheLongestBinaryGap();
+        assertThat(binaryGap, is(0));
+    }
+
+    @Test
+    public void returns5WhenTheNumberis1041() {
+        BinaryGapCodingTask binaryGapCodingTask = new BinaryGapCodingTask(1041);
+        Integer binaryGap = binaryGapCodingTask.getTheLongestBinaryGap();
+        assertThat(binaryGap, is(5));
+    }
+}
