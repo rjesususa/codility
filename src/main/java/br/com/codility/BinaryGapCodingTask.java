3c27ac7c5dae8a17c66d5dbac293069debd29bf6
Rafael Neves <965365+rjesususa@users.noreply.github.com>
2019-05-05 20:50:40 -0400

    implemented solution to binary gap problem

--- src/main/java/br/com/codility/BinaryGapCodingTask.java
+++ src/main/java/br/com/codility/BinaryGapCodingTask.java
@@ -7,12 +7,36 @@ public class BinaryGapCodingTask {
 
     int number;
 
-    public BinaryGapCodingTask(int number){
+    public BinaryGapCodingTask(int number) {
 
         this.number = number;
     }
 
     public Integer getTheLongestBinaryGap() {
-        return null;
+        final String binaryValue = Integer.toBinaryString(number);
+
+        int maxSize = 0;
+        int size = 0;
+        boolean fistBinaryGap = true;
+        for (int i = 0; i < binaryValue.length(); i++) {
+            Character value = binaryValue.charAt(i);
+            if (value.equals('1')) {
+
+                if (fistBinaryGap) {
+                    size = 0;
+                    fistBinaryGap = false;
+                    continue;
+                }
+
+                if (size > maxSize) {
+                    maxSize = size;
+                }
+                size = 0;
+            } else {
+                size++;
+            }
+        }
+
+        return maxSize;
     }
 }
