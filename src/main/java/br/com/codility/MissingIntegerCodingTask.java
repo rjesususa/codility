34591112c1eda8574fd4a322e4103ac544546eba
Rafael Neves <965365+rjesususa@users.noreply.github.com>
2019-05-09 18:15:50 -0400

    Missing Integer task solved with O (N ) solution

--- src/main/java/br/com/codility/MissingIntegerCodingTask.java
+++ src/main/java/br/com/codility/MissingIntegerCodingTask.java
@@ -4,6 +4,7 @@ package br.com.codility;
  * Created by rjesus on 08/05/19.
  */
 public class MissingIntegerCodingTask {
+    private static final int MAX_RANGE = 100000001;
     int[] numbers;
 
     public MissingIntegerCodingTask(int[] numbers) {
@@ -11,6 +12,20 @@ public class MissingIntegerCodingTask {
     }
 
     public int retrieveTheSmallestPositiveMissingNumber() {
-        return 0;
+
+        boolean[] isARepeatableNumber = new boolean[MAX_RANGE];
+
+        for (int number : numbers) {
+            if (number <= 0) continue;
+
+            if (!isARepeatableNumber[number]) {
+                isARepeatableNumber[number] = true;
+            }
+        }
+
+        for (int i = 1; i < isARepeatableNumber.length; i++) {
+            if (!isARepeatableNumber[i]) return i;
+        }
+        return MAX_RANGE;
     }
 }
--- src/test/java/br/com/codility/MissingIntegerCodingTaskTest.java
+++ src/test/java/br/com/codility/MissingIntegerCodingTaskTest.java
@@ -18,9 +18,51 @@ public class MissingIntegerCodingTaskTest {
     }
 
     @Test
-    public void returns1WhenTheArrayContainsNegativeAndZero() {
-        MissingIntegerCodingTask missingIntegerCodingTask = new MissingIntegerCodingTask(new int[]{4, 5, 6, 3, 2, 0, -2, -40, -1, 7, 10, -50, 8, -44, 9});
+    public void returns6WhenTheArrayContainsNegativeAndZero() {
+        MissingIntegerCodingTask missingIntegerCodingTask = new MissingIntegerCodingTask(new int[]{4, 5, 1, 3, 2, 0, -2, -40, -1, 7, 10, -50, 8, -44, 9});
+        int missingNumber = missingIntegerCodingTask.retrieveTheSmallestPositiveMissingNumber();
+        assertThat(missingNumber, is(6));
+    }
+
+    @Test
+    public void returns4WhenTheArrayContainsNegativeAndZeroAndRepeatableNumbers() {
+        MissingIntegerCodingTask missingIntegerCodingTask = new MissingIntegerCodingTask(new int[]{1, 5, 6, 3, 2, 2, 0, -2, -40, -1, 7, 10, -1, -10, -10 - 50, 8, -44, 9});
+        int missingNumber = missingIntegerCodingTask.retrieveTheSmallestPositiveMissingNumber();
+        assertThat(missingNumber, is(4));
+    }
+
+    @Test
+    public void returns6TheNextNaturalNumberWhenTheArrayContainsAllNumbers() {
+        MissingIntegerCodingTask missingIntegerCodingTask = new MissingIntegerCodingTask(new int[]{4, 2, 5, 1, 3});
+        int missingNumber = missingIntegerCodingTask.retrieveTheSmallestPositiveMissingNumber();
+        assertThat(missingNumber, is(6));
+    }
+
+    @Test
+    public void returns1WhenTheArrayIsMissingMoreThanOneNumberFor4562() {
+        MissingIntegerCodingTask missingIntegerCodingTask = new MissingIntegerCodingTask(new int[]{4, 5, 6, 2});
         int missingNumber = missingIntegerCodingTask.retrieveTheSmallestPositiveMissingNumber();
         assertThat(missingNumber, is(1));
+    }
+
+    @Test
+    public void returns1WhenTheArrayContainsLargeNumbers() {
+        MissingIntegerCodingTask missingIntegerCodingTask = new MissingIntegerCodingTask(new int[]{53, 54, 56, 55});
+        int missingNumber = missingIntegerCodingTask.retrieveTheSmallestPositiveMissingNumber();
+        assertThat(missingNumber, is(1));
+    }
+
+    @Test
+    public void returns1WhenTheArrayContainsLargeNumbersAndMissingNumberAmongThem() {
+        MissingIntegerCodingTask missingIntegerCodingTask = new MissingIntegerCodingTask(new int[]{53, 56, 55});
+        int missingNumber = missingIntegerCodingTask.retrieveTheSmallestPositiveMissingNumber();
+        assertThat(missingNumber, is(1));
+    }
+
+    @Test
+    public void returns6WhenTheArrayContainsLargeNumbersAndMissingNumberAmongThem() {
+        MissingIntegerCodingTask missingIntegerCodingTask = new MissingIntegerCodingTask(new int[]{53, 56, 55,1,5,2,5,3,-1,5,4});
+        int missingNumber = missingIntegerCodingTask.retrieveTheSmallestPositiveMissingNumber();
+        assertThat(missingNumber, is(6));
     }
 }
