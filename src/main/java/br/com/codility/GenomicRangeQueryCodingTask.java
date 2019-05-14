ab0253908d5f3e679fd8c3114aef1a3f30232065
Rafael Neves <965365+rjesususa@users.noreply.github.com>
2019-05-14 15:53:01 -0400

    Better solution founded in https://stackoverflow.com/questions/19552754/java-codility-training-genomic-range-query with time complexity O(N + M) using prefix sums approach

--- src/main/java/br/com/codility/GenomicRangeQueryCodingTask.java
+++ src/main/java/br/com/codility/GenomicRangeQueryCodingTask.java
@@ -18,16 +18,42 @@ public class GenomicRangeQueryCodingTask {
     public int[] findTheMinimalImpactFactor() {
         int[] minimalImpactFactor = new int[positionP.length];
 
+        final int lengthOfSequenceDNA = sequenceDNA.length();
+        int[] nucleotidesTypeA = new int[lengthOfSequenceDNA + 1];
+        int[] nucleotidesTypeC = new int[lengthOfSequenceDNA + 1];
+        int[] nucleotidesTypeG = new int[lengthOfSequenceDNA + 1];
+
+        int sumOfA, sumOfC, sumOfG;
+
+        for (int i = 0; i < lengthOfSequenceDNA; i++) {
+            sumOfA = 0;
+            sumOfC = 0;
+            sumOfG = 0;
+            final char pieceOfDNA = sequenceDNA.charAt(i);
+            if (pieceOfDNA == 'A') {
+                sumOfA = 1;
+            }
+            if (pieceOfDNA == 'C') {
+                sumOfC = 1;
+            }
+            if (pieceOfDNA == 'G') {
+                sumOfG = 1;
+            }
+
+            nucleotidesTypeA[i + 1] = nucleotidesTypeA[i] + sumOfA;
+            nucleotidesTypeC[i + 1] = nucleotidesTypeC[i] + sumOfC;
+            nucleotidesTypeG[i + 1] = nucleotidesTypeG[i] + sumOfG;
+        }
+
         for (int i = 0; i < positionP.length; i++) {
             int startsFromPosition = positionP[i];
             int endsInPosition = positionQ[i] + 1;
-            String pieceOfDNA = sequenceDNA.substring(startsFromPosition, endsInPosition);
 
-            if (pieceOfDNA.contains("A")) {
+            if (nucleotidesTypeA[endsInPosition] - nucleotidesTypeA[startsFromPosition] > 0) {
                 minimalImpactFactor[i] = 1;
-            } else if (pieceOfDNA.contains("C")) {
+            } else if (nucleotidesTypeC[endsInPosition] - nucleotidesTypeC[startsFromPosition] > 0) {
                 minimalImpactFactor[i] = 2;
-            } else if (pieceOfDNA.contains("G")) {
+            } else if (nucleotidesTypeG[endsInPosition] - nucleotidesTypeG[startsFromPosition] > 0) {
                 minimalImpactFactor[i] = 3;
             } else {
                 minimalImpactFactor[i] = 4;
--- src/test/java/br/com/codility/GenomicRangeQueryCodingTaskTest.java
+++ src/test/java/br/com/codility/GenomicRangeQueryCodingTaskTest.java
@@ -27,10 +27,38 @@ public class GenomicRangeQueryCodingTaskTest {
     }
 
     @Test
+    public void shouldReturnsValue2WhenDNASequenceIsCWithP0ANdQ0() throws Exception {
+        GenomicRangeQueryCodingTask genomicRangeQueryCodingTask = new GenomicRangeQueryCodingTask("C", new int[]{0}, new int[]{0});
+        int[] minimalImpactFactor = genomicRangeQueryCodingTask.findTheMinimalImpactFactor();
+        assertThat(minimalImpactFactor, is(equalTo(new int[]{2})));
+    }
+
+    @Test
+    public void shouldReturnsValue3WhenDNASequenceIsGWithP0ANdQ0() throws Exception {
+        GenomicRangeQueryCodingTask genomicRangeQueryCodingTask = new GenomicRangeQueryCodingTask("G", new int[]{0}, new int[]{0});
+        int[] minimalImpactFactor = genomicRangeQueryCodingTask.findTheMinimalImpactFactor();
+        assertThat(minimalImpactFactor, is(equalTo(new int[]{3})));
+    }
+
+    @Test
+    public void shouldReturnsValue4WhenDNASequenceIsTWithP0ANdQ0() throws Exception {
+        GenomicRangeQueryCodingTask genomicRangeQueryCodingTask = new GenomicRangeQueryCodingTask("T", new int[]{0}, new int[]{0});
+        int[] minimalImpactFactor = genomicRangeQueryCodingTask.findTheMinimalImpactFactor();
+        assertThat(minimalImpactFactor, is(equalTo(new int[]{4})));
+    }
+
+    @Test
     public void shouldReturnsValue1WhenDNASequenceIsAWithP0ANdQ1() throws Exception {
         GenomicRangeQueryCodingTask genomicRangeQueryCodingTask = new GenomicRangeQueryCodingTask("AT", new int[]{0}, new int[]{1});
         int[] minimalImpactFactor = genomicRangeQueryCodingTask.findTheMinimalImpactFactor();
         assertThat(minimalImpactFactor, is(equalTo(new int[]{1})));
+    }
+
+    @Test
+    public void shouldReturnsValue112WhenDNASequenceIsACWithP001ANdQ011() throws Exception {
+        GenomicRangeQueryCodingTask genomicRangeQueryCodingTask = new GenomicRangeQueryCodingTask("AC", new int[]{0, 0, 1}, new int[]{0, 1, 1});
+        int[] minimalImpactFactor = genomicRangeQueryCodingTask.findTheMinimalImpactFactor();
+        assertThat(minimalImpactFactor, is(equalTo(new int[]{1, 1, 2})));
     }
 
 }
