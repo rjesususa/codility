8e93bb43b99feb76d1add22bea536cdba1d15d45
Rafael Neves <965365+rjesususa@users.noreply.github.com>
2019-05-21 21:01:00 -0400

    Added constant variable

--- src/main/java/br/com/codility/DistinctValueCodingTask.java
+++ src/main/java/br/com/codility/DistinctValueCodingTask.java
@@ -4,6 +4,7 @@ package br.com.codility;
  * Created by rjesus on 21/05/19.
  */
 public class DistinctValueCodingTask {
+    private static final int LIMIT_OF_RANGE = 1000000;
     int[] integers;
 
     public DistinctValueCodingTask(int[] integers) {
@@ -11,8 +12,8 @@ public class DistinctValueCodingTask {
     }
 
     public int countDistinctValues() {
-        boolean[] negativeNumbers = new boolean[1000000];
-        boolean[] positiveNumbers = new boolean[1000000];
+        boolean[] negativeNumbers = new boolean[LIMIT_OF_RANGE];
+        boolean[] positiveNumbers = new boolean[LIMIT_OF_RANGE];
         int countDistinctValues = 0;
 
         for (int i : integers) {
