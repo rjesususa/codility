1afde0e7abcfadf75edc2761fe5edf5ff161c4a5
Rafael Neves <965365+rjesususa@users.noreply.github.com>
2019-05-21 20:00:07 -0400

    Added test for class Distinct Value

---
+++ src/main/java/br/com/codility/DistinctValueCodingTask.java
@@ -0,0 +1,16 @@
+package br.com.codility;
+
+/**
+ * Created by rjesus on 21/05/19.
+ */
+public class DistinctValueCodingTask {
+    int[] integers;
+
+    public DistinctValueCodingTask(int[] integers) {
+        this.integers = integers;
+    }
+
+    public int countDistinctValues() {
+        return 0;
+    }
+}
---
+++ src/test/java/br/com/codility/DistinctValueCodingTaskTest.java
@@ -0,0 +1,20 @@
+package br.com.codility;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+/**
+ * Created by rjesus on 21/05/19.
+ */
+public class DistinctValueCodingTaskTest {
+
+    @Test
+    public void returns3WhenTheArrayContains211231() throws Exception {
+        DistinctValueCodingTask distinctValueCodingTask = new DistinctValueCodingTask(new int[]{2,1,1,2,3,1});
+        int distinctValues = distinctValueCodingTask.countDistinctValues();
+        assertThat(distinctValues, is(3));
+    }
+
+}
